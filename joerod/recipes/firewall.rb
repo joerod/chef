b1ee2d59bc9e000177adc9cc52601af0924f3be9
Joe Rodriguez <joerod@gmail.com>
2018-04-25 01:34:33 -0400

    added enable remote desktop

--- joerod/recipes/firewall.rb
+++ joerod/recipes/firewall.rb
@@ -1,3 +1,4 @@
+# adds windows fw exemption
 [{name: 'RemoteDesktop-UserMode-In-TCP', displayname: 'Remote Desktop - User Mode (TCP-In)' },
  {name: 'RemoteDesktop-UserMode-In-UDP', displayname: 'Remote Desktop - User Mode (UDP-In)' }].each do |fwrule|
   dsc_script 'Set RDP Firewall Rule' do
@@ -12,4 +13,16 @@
         EOH
     imports 'xNetworking'
   end
+end
+
+dsc_script 'Enable Remote Desktop' do
+    code <<-EOH
+        Registry 'Enable Remote Desktop'
+        {
+            Key         = "HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server"
+            ValueName   = "fDenyTSConnections"
+            ValueData   = "0"
+            ValueType   = "DWORD"
+        }
+    EOH
 end
